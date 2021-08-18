a0a90f336b4e9661c00068943271b829606cd4ba
oguzhan-hub <oguz.kus.code@gmail.com>
2021-08-19 00:09:06 +0300

    function change for try F5

--- 310_deneme/Form1.cs
+++ 310_deneme/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _310_deneme
 {
@@ -14,12 +15,24 @@ namespace _310_deneme
     {
         public Form1()
         {
+            
             InitializeComponent();
+            //prepare diplay screen (cleaning)
+            tabControlRegister.Hide();
+
+            //************Read it from text area.***********\\
+            string userRecordPath = @"C:\Users\Oğuzhan Kuş\source\repos\310_deneme\UserRecord.txt";
+
+            //variables for user records
+            string[] userInfoArray = File.ReadAllLines(userRecordPath);
+            List<string> userInfoList = new List<string>();             //this for checking login
+            userInfoList = File.ReadAllLines(userRecordPath).ToList();  //this for recording new entries on text (easy append in string format)
         }
 
-        private void btn_deneme_Click(object sender, EventArgs e)
+        private void btnRegister_Click(object sender, EventArgs e)
         {
-
+            groupLogin.Hide();
+            tabControlRegister.Show();
         }
     }
 }
